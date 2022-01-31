ce24439427642a7e90eea3f1c81e1380af4c0678
fahrimaulana <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2022-01-31 07:40:29 +0700

    final

--- src/Currency.java
+++ src/Currency.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.text.DecimalFormat;
 public class Currency {
@@ -7,7 +6,7 @@ public class Currency {
     public static void main(String[] args) {
 
         //Inisialisai variabel
-        double amount, dollar, pound, code, euro, yen, ringgit, rupee;
+        double amount, dollar, pound, code, euro, yen, ringgit, rupee, rupiah;
 
         DecimalFormat f = new DecimalFormat("##.##");
         System.out.println("Welcome To Currency Converter");
@@ -21,7 +20,30 @@ public class Currency {
         //Percabangan untuk memulai perhitungan konversi mata uang
 
         if (code == 1){
-            
+            rupiah = 192.55 * amount;
+            System.out.println(amount + " Rupee adalah "+rupiah+ " Rupiah");
+        }
+        else if (code == 2){
+            rupiah = 14.328 * amount;
+            System.out.println(amount + " Dollar adalah "+rupiah + " Rupiah");
+        }
+        else if (code == 3){
+            rupiah = 19278 * amount;
+            System.out.println(amount + " Pound adalah "+rupiah + " Rupiah");
+        }
+        else if (code == 4){
+            rupiah = 16.207 * amount;
+            System.out.println(amount + " Euro adalah "+rupiah + " Rupiah");
+        }
+        else if (code == 5){
+            rupiah = 124.41 * amount;
+            System.out.println(amount + " Yen adalah "+rupiah+ " Rupiah");
+        }
+        else if (code == 6){
+            rupiah = 3.423 * amount;
+            System.out.println(amount + " Ringgit adalah "+rupiah + "Rupiah");
+        }else{
+            System.out.println("Parameter Tidak Valid");
         }
     }
 }
