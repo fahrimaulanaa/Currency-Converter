a51c0bf8cb0990569e27560b42c75c3fa082cbdf
fahrimaulana <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2022-01-31 07:20:02 +0700

    iyah

--- src/Currency.java
+++ src/Currency.java
@@ -1,5 +1,27 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.text.DecimalFormat;
 public class Currency {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        //Inisialisai variabel
+        double amount, dollar, pound, code, euro, yen, ringgit, rupee;
+
+        DecimalFormat f = new DecimalFormat("##.##");
+        System.out.println("Welcome To Currency Converter");
+        System.out.println("Mata uang apa yang anda ingin rubah ke rupiah?");
+        System.out.println("1:Rupee \t2:Dollar \t3:Pound \t4:Euro  \t5:Yen    \t6:Rinngit");
+        code = sc.nextInt();
+
+        System.out.println("Berapa nominal yang anda ingin konversi: ");
+        amount = sc.nextDouble();
+
+        //Percabangan untuk memulai perhitungan konversi mata uang
+
+        if (code == 1){
+            
+        }
     }
 }
